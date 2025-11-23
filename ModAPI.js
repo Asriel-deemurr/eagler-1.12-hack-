f02431269af7832080b6aaa4336243948118d145
Asriel-deemurr <kazutaki843a1@gmail.com>
2025-11-23 18:35:48 +0900

    Update ModAPI.js

--- ModAPI.js
+++ ModAPI.js
@@ -117,7 +117,57 @@ function initAPI(version) {
     });
     ModAPI.globals.toUpdate = [];
   };
+
+  // ここまでが元からある ModAPI のコード
+
+
+
+// --- ここからあなたのX-rayコード ---
+/* Coalest xray mod */
+
+/ (function () {
+    var enabled = false
+    ModAPI.addEventListener("key", function(ev){
+        if(ev.key == 45){// the "x" key
+          if(enabled){
+                disable()
+                enabled = false
+          } else{
+                update(); //Trigger the coal xray.
+                enabled = true
+          }
+        }
+    })
+    var targets = ["diamond_block","diamond_ore","gold_block","gold_ore","iron_block","iron_ore","coal_block","coal_ore","emerald_ore","emerald_block","redstone_ore","redstone_block","lapis_ore","lapis_block","chest","furnace","lit_furnace","ender_chest"]; 
+    var allblocks = Object.keys(ModAPI.blocks);
+    function update() {
+      ModAPI.displayToChat({msg: "xray Enabled!"})
+      allblocks.forEach(block=>{
+        if (targets.includes(block)) {
+          ModAPI.blocks[block].forceRender = true;
+          ModAPI.blocks[block].reload();
+        } else if (ModAPI.blocks[block] && ("noRender" in ModAPI.blocks[block])) {
+          ModAPI.blocks[block].noRender = true;
+          ModAPI.blocks[block].reload();
+        }
+      });
+      ModAPI.reloadchunks()
+    }
+    function disable(){
+      ModAPI.displayToChat({msg: "xray Disabled!"})
+      allblocks.forEach(block=>{
+        if (ModAPI.blocks[block] && ("noRender" in ModAPI.blocks[block])) { 
+          ModAPI.blocks[block].noRender = false;
+          ModAPI.blocks[block].reload();
+        }
+      });
+      ModAPI.reloadchunks()
+    }
+})();
+// --- ここまでX-ray ---
+
   
 
   window.ModAPI = ModAPI;
+
 }
